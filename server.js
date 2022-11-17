0aab51bf8c9079aece2601866002b47474554878
derek helgoe <derek1896@gmail.com>
2022-11-17 09:41:10 -0600

    done

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const fs = require("fs");
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
-//const notes = require('./db/db.json');
 
-//const htmlRoutes = require("./routes/htmlRoutes");
+
+
 const apiRoutes = require("./routes/apiRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/notes", apiRoutes);
-//app.use("/", htmlRoutes);
+
 
 app.use(express.static('public'));
 
-//app.get('/api/notes', (req, res) => res.json(notes));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -24,9 +23,6 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
